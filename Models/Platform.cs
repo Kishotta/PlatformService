c483001bb948fb5a687276e5e0987d0dc50c021c
Connor Eaves <kishotta@gmail.com>
2022-05-10 17:19:31 -0500

    Add Platform Model

---
+++ Models/Platform.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Publisher { get; set; }
+
+    [Required]
+    public string Cost { get; set; }
+
+    public Platform(string name, string publisher, string cost)
+    {
+        Name = name;
+        Publisher = publisher;
+        Cost = cost;
+    }
+}
